5a3084aeb97655c69b0fe7aad0c3104a77dfb4d3
jalu.wibowo <jalu.wibowo@sekolahmu.co.id>
2023-02-04 17:05:30 +0700

    fix: Meta Tag Not Showing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createHead } from '@vueuse/head'
 
 import App from './App.vue'
 import router from './router'
@@ -8,10 +7,8 @@ import router from './router'
 import './assets/base.css'
 
 const app = createApp(App)
-const head = createHead()
 
 app.use(createPinia())
 app.use(router)
-app.use(head)
 
 app.mount('#app')
